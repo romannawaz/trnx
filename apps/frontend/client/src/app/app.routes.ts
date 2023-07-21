81f67c533602a06b8b8b24c2cc7c87b128624776
romannawaz <romannawaz01@gmail.com>
2023-07-21 12:46:09 +0300

    [frontend/client][feat] added layout as default route

--- apps/frontend/client/src/app/app.routes.ts
+++ apps/frontend/client/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Route } from '@angular/router';
+import { UiLayoutComponent } from '@trnx/client/ui/layout';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  { path: '', component: UiLayoutComponent, children: [], title: 'Trnx' },
+];
