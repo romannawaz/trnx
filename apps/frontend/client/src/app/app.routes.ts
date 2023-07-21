15973cbed95c38eb1f802779b2585ffa0547146f
romannawaz <romannawaz01@gmail.com>
2023-07-21 15:18:21 +0300

    [frontend/client][feat] home page route

--- apps/frontend/client/src/app/app.routes.ts
+++ apps/frontend/client/src/app/app.routes.ts
@@ -2,5 +2,18 @@ import { Route } from '@angular/router';
 import { UiLayoutComponent } from '@trnx/client/ui/layout';
 
 export const appRoutes: Route[] = [
-  { path: '', component: UiLayoutComponent, children: [], title: 'Trnx' },
+  {
+    path: '',
+    component: UiLayoutComponent,
+    children: [
+      {
+        title: 'Trnx',
+        path: '',
+        loadComponent: () =>
+          import('@trnx/client/content/home/page').then(
+            (c) => c.HomePageComponent
+          ),
+      },
+    ],
+  },
 ];
