d529b1e936fb7bfd1a10a44f2187dc9023e0b2e4
romannawaz <romannawaz01@gmail.com>
2023-07-21 17:45:10 +0300

    [frontend/client][feat] provided content-type interceptor

--- apps/frontend/client/src/app/app.config.ts
+++ apps/frontend/client/src/app/app.config.ts
@@ -1,10 +1,20 @@
 import { ApplicationConfig } from '@angular/core';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import {
   provideRouter,
   withEnabledBlockingInitialNavigation,
 } from '@angular/router';
 import { appRoutes } from './app.routes';
 
+import { ContentTypeInterceptor } from '@trnx/core/api/interceptors/content-type';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation())],
+  providers: [
+    provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ContentTypeInterceptor,
+      multi: true,
+    },
+  ],
 };
