c5bb3eecc5d0ea4f972f2e5c800c320c644d41ff
romannawaz <romannawaz01@gmail.com>
2023-07-22 18:15:06 +0300

    [frontend/client][feat] provided RootStoreModule

--- apps/frontend/client/src/app/app.config.ts
+++ apps/frontend/client/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
 import {
   provideRouter,
@@ -8,6 +8,7 @@ import { appRoutes } from './app.routes';
 
 import { ContentTypeInterceptor } from '@trnx/core/api/interceptors/content-type';
 import { WithCretendialsInterceptor } from '@trnx/core/api/interceptors/with-credentials';
+import { RootStoreModule } from '@trnx/core/store/root';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -23,5 +24,6 @@ export const appConfig: ApplicationConfig = {
       useClass: WithCretendialsInterceptor,
       multi: true,
     },
+    importProvidersFrom(RootStoreModule),
   ],
 };
