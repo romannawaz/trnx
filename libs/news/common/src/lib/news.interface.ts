5a41b04eaf4e5f1acdb14642c57e718a87d71e57
romannawaz <romannawaz01@gmail.com>
2023-07-23 13:14:48 +0300

    [news/common][feat] NewsEntity interface, added _id to UpdateNews type

--- libs/news/common/src/lib/news.interface.ts
+++ libs/news/common/src/lib/news.interface.ts
@@ -7,9 +7,24 @@ export interface INews {
   updatedAt: Date;
 }
 
+export interface NewsEntity {
+  _id: string;
+  title: string;
+  description: string;
+  views: number;
+  createdAt: Date;
+  updatedAt: Date;
+
+  newsRemoveRun: boolean;
+  newsRemoveError: Record<string, any> | null;
+
+  newsChangeRun: boolean;
+  newsChangeError: Record<string, any> | null;
+}
+
 export interface CreateNews {
   title: string;
   description: string;
 }
 
-export type UpdateNews = Partial<INews>;
+export type UpdateNews = Partial<CreateNews> & { _id: string };
