c127bc6b1627f78d5fa5d02ff711719f909a4637
romannawaz <romannawaz01@gmail.com>
2023-07-24 10:30:31 +0300

    [core/store/utils][feat] createEntityState

--- libs/core/store/utils/src/lib/creator.util.ts
+++ libs/core/store/utils/src/lib/creator.util.ts
@@ -1,5 +1,7 @@
+import { Dictionary, EntityState } from '@ngrx/entity';
 import { Store } from '@ngrx/store';
 import { MockStore } from '@ngrx/store/testing';
+import { Entity } from '@trnx/core/common';
 
 export function createStore<S = Record<string, any>, P = Record<string, any>>(
   key: string,
@@ -16,6 +18,23 @@ export function createState<S = Record<string, any>>(
   params: Partial<S> = {}
 ): S {
   return { ...initialState, ...params };
+}
+
+export function createEntityState<S extends Entity = any>(
+  data: S[]
+): EntityState<S> {
+  const ids: number[] = [];
+  const entities: Dictionary<S> = {};
+
+  for (const item of data) {
+    ids.push(Number(item._id));
+    entities[item._id] = item;
+  }
+
+  return {
+    ids,
+    entities,
+  };
 }
 
 export function setMockStore<S = Record<string, any>>(
