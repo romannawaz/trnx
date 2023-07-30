5c0f9ac31a622cb7f3aa606a4d5903a2a25ee159
romannawaz <romannawaz01@gmail.com>
2023-07-30 15:51:35 +0300

    [news][refactor] moved stubs to 'common' lib

--- libs/news/common/src/lib/news.stub.ts
+++ libs/news/common/src/lib/news.stub.ts
@@ -1,10 +1,46 @@
-import { INews } from './news.interface';
+import { Entity } from '@trnx/core/common';
+import { CreateNews, INews, News, UpdateNews } from './news.interface';
 
-export const newsStub: INews = {
-  _id: '64b2e3de880255badfc4250a',
-  title: 'title',
-  description: 'description',
-  views: 0,
-  createdAt: new Date('2023-07-15T18:22:22.321Z'),
-  updatedAt: new Date('2023-07-15T18:22:22.321Z'),
+export const NEWS_DTO_STUB: INews = {
+  _id: '123',
+  title: 'Awesome title',
+  description: 'Such a great description',
+  views: 10,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+};
+
+export const NEWS_STUB: News = {
+  ...NEWS_DTO_STUB,
+  newsRemoveRun: false,
+  newsRemoveError: null,
+  newsChangeRun: false,
+  newsChangeError: null,
+};
+
+const NEWS_STUB_SECOND: News = {
+  ...NEWS_STUB,
+  _id: '456',
+};
+
+export const NEWS_ARRAY_STUB: News[] = [NEWS_STUB, NEWS_STUB_SECOND];
+
+export const NEWS_ERROR_STUB: Record<string, any> = {
+  code: 0,
+  message: 'Failure load news',
+};
+
+export const ENTITY_STUB: Entity = {
+  _id: NEWS_STUB._id,
+};
+
+export const NEW_NEWS_STUB: CreateNews = {
+  title: NEWS_STUB.title,
+  description: NEWS_STUB.description,
+};
+
+export const UPDATE_NEWS_STUB: UpdateNews = {
+  _id: NEWS_STUB._id,
+  title: NEWS_STUB.title,
+  description: NEWS_STUB.description,
 };
